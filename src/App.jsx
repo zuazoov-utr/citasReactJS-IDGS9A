8baaecc1e1d8964084fc06f6cf7e322a133e95e3
Vidal Zuazo <vidal.zuazo@utr.edu.mx>
2026-09-22 10:05:07 -0600

    Create header and button structure and first useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+import './css/App.css';
 
 function App() {
+  const [visible, setVisible] = useState(false);
 
   return (
-    <>
-     <h1>Hola Mundo: Citas JS</h1>
-    </>
+    <main className="container">
+      <h1 className="titulo">
+        Administrador de Citas <span className="titulo-bold">Veterinario</span>
+      </h1>
+      <button
+        type='button'
+        className='btn-nueva-cita'
+        onClick={() => setVisible(true)}
+      >
+        <span className='btn-texto-nueva-cita'>Nueva Cita</span>
+      </button>
+    </main>
   )
 }
 
